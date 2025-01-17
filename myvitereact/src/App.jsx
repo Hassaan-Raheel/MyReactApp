a12f83bbc4e9a05d7a89c6ab0779e2ede3ae2746
Syed Hassaan Raheel <syedhassaanraheel@MacBook-Air.local>
2025-01-17 22:05:21 +0500

    Front end

--- myvitereact/src/App.jsx
+++ myvitereact/src/App.jsx
@@ -2,9 +2,11 @@ import Myapp from "./Myapp"
 
 function App() {
 
+  const username = "Zelle Solutions"
+
   return (
     <>
-    <h1>Hey there</h1>
+    <h1>Hey there Welcome to {username}</h1>
    <Myapp/>
 
    <h3>See you next time</h3>
--- myvitereact/src/main.jsx
+++ myvitereact/src/main.jsx
@@ -1,9 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client' 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
+
+function  Myapp2(){
+  return(
+    <div>
+      <h1>Zelle Solutions App</h1>
+      </div>
+  )
+}
+const anotherElement = (
   
+   <a href="https://google.com" target='_blank'>Visit Google</a>
+
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    //<Myapp2 />
+    //anotherElement
+    //reactElement
+    <App />
 )
